e8f5b47fba10bc4eded85c2ca62c08f76a9a52ff
ralf <ralfbawg@gmail.com>
2019-08-02 01:22:56 +0800

    初始导入

--- web/src/main/java/com/ralf/cardmanager/spider/task/divvypay/operation/BaseOperation.java
+++ web/src/main/java/com/ralf/cardmanager/spider/task/divvypay/operation/BaseOperation.java
@@ -1,12 +1,29 @@
 package com.ralf.cardmanager.spider.task.divvypay.operation;
 
+import com.jeesite.common.web.http.HttpClientUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+
 public abstract class BaseOperation {
+
+
+
+
+    private BaseOperationRequest request;
+
     private String defaultUserAgent = "";
 
     private String defaultUrl = "https://www.divvypay.co/js/graph";
 
     public abstract BaseOperationResponse excute();
 
+
     public abstract void persistent();
+
 
 }
---
+++ web/src/main/java/com/ralf/cardmanager/spider/task/divvypay/operation/GetVirtualCardByBudget.java
@@ -0,0 +1,13 @@
+package com.ralf.cardmanager.spider.task.divvypay.operation;
+
+public class GetVirtualCardByBudget extends BaseOperation {
+    @Override
+    public BaseOperationResponse excute() {
+        return null;
+    }
+
+    @Override
+    public void persistent() {
+
+    }
+}
