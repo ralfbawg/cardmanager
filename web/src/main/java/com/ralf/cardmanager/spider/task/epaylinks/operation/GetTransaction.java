24e0e3924621265f2f8059036d5d43bc8124d96a
ralf <ralfbawg@gmail.com>
2019-10-16 00:49:44 +0800

    加了好多东东

--- web/src/main/java/com/ralf/cardmanager/spider/task/epaylinks/operation/GetTransaction.java
+++ web/src/main/java/com/ralf/cardmanager/spider/task/epaylinks/operation/GetTransaction.java
@@ -2,12 +2,19 @@ package com.ralf.cardmanager.spider.task.epaylinks.operation;
 
 import com.ralf.cardmanager.spider.task.epaylinks.config.EpaylinksSiteConfig;
 import com.ralf.cardmanager.spider.task.epaylinks.operation.base.BaseEpaylinksOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class GetTransaction extends BaseEpaylinksOperation {
+    @Autowired
     public GetTransaction(EpaylinksSiteConfig config) {
         super(config);
         this.url = host+"/account/record.do?method=qryTradeList";
     }
-
+    public GetTransaction init(String amount) throws Exception{
+        super.init(new String[]{
+                amount
+        });
+        return this;
+    }
 
 }
